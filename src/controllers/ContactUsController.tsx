3c167fde60bb0be7c6381b96595e744e46fec975
azad ul kabir <azadulkabir10@gmail.com>
2023-12-31 21:55:23 +0600

    [dev] contact us page banner and info section done

---
+++ src/controllers/ContactUsController.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class ContactUsController extends Component {
+  render() {
+    return (
+      <div>ContactUsController</div>
+    )
+  }
+}
