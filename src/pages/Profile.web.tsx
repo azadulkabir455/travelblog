3f3175298736d0d8da923c393daa5ef9e9888af1
azad ul kabir <azadulkabir10@gmail.com>
2024-01-06 23:12:12 +0600

    [dev] modify profile page design

--- src/pages/Profile.web.tsx
+++ src/pages/Profile.web.tsx
@@ -11,31 +11,42 @@ export default class Profile extends Component {
       <>
         <Navbar />
         <div className="profileContainer py-4">
-          <div className="row g-5">
+          <div className="row g-4">
             <div className="col-12 col-lg-4">
               <div className="userProfile shadow-lg rounded-3 p-4 position-sticky">
-                <img
-                  src="https://shorturl.at/akAHL"
-                  alt=""
-                  className="rounded-circle mx-auto d-block"
-                />
-                <h3 className="text-center mt-4 mb-0 text-secondary">
+                <div className="fileImageContainer position-relative d-block">
+                  <img
+                    src="https://shorturl.at/akAHL"
+                    className="image-fluid rounded-circle d-block mx-auto object-fit-cover"
+                    alt="profile pic"
+                  />
+                  <Link to="/editprofile" className="editIcon bg-light d-inline-block px-2 py-1 border border-3 border-body-secodary m-2 rounded-circle position-absolute">
+                    <Edit size="16px" color="#d16b86" />
+                  </Link>
+                </div>
+                <h3 className="text-center mt-3 mb-0 text-secondary fw-bold">
                   Jhone Ibrahim
                 </h3>
                 <p className="text-center text-body-secondary">
                   jhone@gmail.com
                 </p>
                 <div className="socialIcon d-flex justify-content-center">
-                  <Facebook size="18px" />
-                  <Linkedin size="18px" />
+                  <Facebook
+                    size="25px"
+                    className="border border-primary rounded-2 p-1"
+                  />
+                  <Linkedin
+                    size="25px"
+                    className="border border-primary rounded-2 p-1"
+                  />
                 </div>
-                <Link
+                {/* <Link
                   to="/editprofile"
                   className="text-center fw-medium d-block mt-3 link-underline link-underline-opacity-0"
                 >
                   <Edit3 size="16px" className="me-1"/>
                   Edit Profile
-                </Link>
+                </Link> */}
               </div>
             </div>
             <div className="col-12 col-lg-7">
